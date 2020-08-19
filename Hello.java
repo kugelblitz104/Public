86ab50b690918af2596542b060cae9042c665178
kugelblitz104 <kugelblitz104@gmail.com>
2020-08-19 13:27:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+Hello
+
+Justin Jones
+8/19/2020
+CMSC 255-002
+
+*/
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Justin Jones");
+		System.out.println("Hello");
+		System.out.println("Testing JDK to make sure everything works");
+		System.out.println("I dont know the version date?");
+		System.out.println("CMSC 255-002");
+		System.out.println();
+	}
+}
